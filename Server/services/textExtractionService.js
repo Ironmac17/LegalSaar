0cb8e13fb116435db24edd75202cf7cabf407e1a
Ironmac17 <nimish4agrawal@gmail.com>
2026-02-03 00:19:52 +0530

    issues fixed

--- Server/services/textExtractionService.js
+++ Server/services/textExtractionService.js
@@ -1,5 +1,8 @@
-const pdfParse = require("pdf-parse").default;
+const path = require("path");
 const Tesseract = require("tesseract.js");
+const pdfParse = require("pdf-parse");
+
+const OCR_LANG_PATH = path.join(__dirname, "..", "ocr-data");
 
 const extractTextFromPDF = async (buffer) => {
   const data = await pdfParse(buffer);
@@ -7,7 +10,10 @@ const extractTextFromPDF = async (buffer) => {
 };
 
 const extractTextFromImage = async (buffer) => {
-  const { data } = await Tesseract.recognize(buffer, "eng");
+  const { data } = await Tesseract.recognize(buffer, "eng", {
+    langPath: OCR_LANG_PATH,
+    cachePath: OCR_LANG_PATH
+  });
   return data.text;
 };
 
